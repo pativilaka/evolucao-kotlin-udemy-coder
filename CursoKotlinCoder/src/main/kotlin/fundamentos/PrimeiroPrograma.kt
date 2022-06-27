293312802f818b5887619871f88be5a18b5bd64b
pativilaka <p.vilaka@gmail.com>
2022-06-27 20:39:51 -0300

    Inclusão do primeiro exercício de fundamentos Kotlin

---
+++ CursoKotlinCoder/src/main/kotlin/fundamentos/PrimeiroPrograma.kt
@@ -0,0 +1,19 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    //ln pula linha
+    println("Primeiro ")
+    println("Programa!")
+
+    println("")
+
+    //não pula a linha
+    print("Primeiro ")
+    print("Programa!")
+
+    println("")
+    println("")
+
+    print("Primeiro ")
+    println("Programa!")
+}
